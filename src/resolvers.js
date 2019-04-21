3a340a260ddacb228bb11ee2899a11323c54f2f2
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-21 11:46:46 +0700

    graphql-sequelize-sample

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,30 @@
+import Product from "./models/product";
+
 export default {
   Query: {
-    hello: () => "Hello, Graphql",
+    products: async () => {
+      const products = await Product.findAll();
+      return products;
+    },
+  },
+  Mutation: {
+    addProduct: async (parent, { data }) => {
+      const result = await Product.create({
+        title: data.title,
+        price: data.price,
+        imageUrl: data.imageUrl,
+        description: data.description,
+      });
+      return result;
+    },
+    editProduct: async (parent, { data }) => {
+      const product = await Product.findByPk(data.id);
+      product.title = data.title;
+      product.price = data.price;
+      product.description = data.description;
+      product.imageUrl = data.imageUrl;
+      await product.save();
+      return product;
+    },
   },
 };
